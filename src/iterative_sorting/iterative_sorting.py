25b1a2d15f394a1ce254955e12664d8c8b9d96ab
haileyhansard <haileyhansard@gmail.com>
2020-09-14 16:02:12 -0700

    Finished Iterative and Searching files, tests passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,18 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
+    #loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
         # TO-DO: swap
         # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -18,8 +20,15 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    n = len(arr)
+    #traverse through all array elements
+    for i in range(n -1):
+        #Last i elements are already in place
+        for j in range(0, n-i-1):
+            #traverse the array from 0 to n-i-1
+            #swap if the element found is GREATER than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 '''
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,8 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    #Your code here
+    for i in range(0, len(arr)):
+        if (arr[i] == target):
+            return i;
     return -1   # not found
 
 
@@ -9,6 +10,25 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    # find the midpoint element
+    # length // 2
+    # keep track of the left and right most points
+    left = 0
+    right = len(arr) - 1 #because len(arr) is not a valid index, since arr indexes start at 0. it would be 0-9 indexes, but len would be 10. so we do 10 - 1 = 9.
 
+    #keep iterating so long as left <= right
+    while left <= right:
+        midpoint = (right + left) //2
+
+        if arr[midpoint] == target:
+            return midpoint
+
+        elif arr[midpoint] > target:
+            #toss out the left side of the array
+            #toss out the midpoint element itself as well
+            right = midpoint - 1
+        else: 
+            left = midpoint + 1
+    #otherwise, we didn't find what we're looking for
 
     return -1  # not found
